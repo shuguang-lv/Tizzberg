0be8763ae2e8940967c02b85253a1259adaddd3e
Desmond <shuguang-lv@outlook.com>
2022-06-25 22:13:24 +0800

    feat: improve styles of login and signup pages

--- src/views/login.vue
+++ src/views/login.vue
@@ -38,75 +38,55 @@ export default {
           this.authError = error
         })
     },
-    toSignUp () {
-        async () => {
-          await this.logOut()
-          this.$router.push({ name: 'login' })
-        }
-    }
   },
 }
 </script>
 
 <template>
   <Layout>
-    <v-container>
-      <div class="text-h4 secondary--text font-weight-regular">
-        Log in
-      </div>
-    </v-container>
+    <div class="text-h4 mb-12 secondary--text font-weight-medium">Log in</div>
 
-    <v-container>
-      <form>
-        <v-text-field
-          v-model="username"
-          :error-messages="usernameErrors"
-          :rules="nameRules"
-          label="Username / Email address"
-          required
-          @input="$v.name.$touch()"
-          @blur="$v.name.$touch()"
-        ></v-text-field>
-        <v-text-field
-          v-model="password"
-          :error-messages="passwordErrors"
-          label="Password"
-          required
-          @input="$v.password.$touch()"
-          @blur="$v.password.$touch()"
-        ></v-text-field>
+    <form>
+      <v-text-field
+        v-model="username"
+        :error-messages="usernameErrors"
+        :rules="nameRules"
+        label="Username / Email address"
+        outlined
+        required
+        clearable
+        class="mb-4"
+      ></v-text-field>
+      <v-text-field
+        v-model="password"
+        :error-messages="passwordErrors"
+        label="Password"
+        outlined
+        required
+        clearable
+        type="password"
+      ></v-text-field>
+      <div class="d-flex justify-space-between align-center mb-8">
         <v-checkbox
           v-model="checkbox"
           :error-messages="checkboxErrors"
           label="Remember Me"
           required
-          @change="$v.checkbox.$touch()"
-          @blur="$v.checkbox.$touch()"
         ></v-checkbox>
-    
-        <v-btn
-          class="mr-4"
-          @click="signIn"
-        >
+        <v-btn depressed large color="primary" @click="tryToLogIn">
           Sign in
         </v-btn>
-      </form>
-    </v-container>
+      </div>
+    </form>
 
-    <v-container>
-      <v-divider
-      ></v-divider>
-      <v-row class="mt-2 ">
-        <v-col class="col-md-5">
-          <div class="secondary--text font-weight-regular">Don't have an account?</div>
-        </v-col>
-        <v-col class="col-md-2">
-          <!-- <a class="secondary--text font-weight-regular" >Sign up</a> -->
-          <router-link :to="{ path: '/signup' }">Sign up</router-link>
-        </v-col>
-      </v-row>
-    </v-container>
+    <v-divider class="mb-8"></v-divider>
 
+    <div class="d-flex">
+      <div class="secondary--text font-weight-regular mr-4">
+        Don't have an account?
+      </div>
+      <router-link :to="{ name: 'signup' }">Sign up</router-link>
+    </div>
   </Layout>
 </template>
 
--- src/views/signup.vue
+++ src/views/signup.vue
@@ -9,122 +9,111 @@ export default {
     valid: true,
     name: '',
     nameRules: [
-      v => !!v || 'Name is required',
-      v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+      (v) => !!v || 'Name is required',
+      (v) => (v && v.length <= 10) || 'Name must be less than 10 characters',
     ],
     email: '',
     emailRules: [
-      v => !!v || 'E-mail is required',
-      v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      (v) => !!v || 'E-mail is required',
+      (v) => /.+@.+\..+/.test(v) || 'E-mail must be valid',
     ],
     password: '',
     passwordRules: [
-      v => !!v || 'Password is required',
-      v => /^(?=.*[a-zA-Z])(?=.*[0-9])[A-Za-z0-9]{8,18}$/.test(v) || 'Password must be valid',
+      (v) => !!v || 'Password is required',
+      (v) =>
+        /^(?=.*[a-zA-Z])(?=.*[0-9])[A-Za-z0-9]{8,18}$/.test(v) ||
+        'Password must be valid',
     ],
     confirm_password: '',
     confirmPasswordRules: [
       // TODO: same password check
-      v => !!v || 'Please Confirm your Password',
-      v => /^(?=.*[a-zA-Z])(?=.*[0-9])[A-Za-z0-9]{8,18}$/.test(v) || 'Password must be valid',
+      (v) => !!v || 'Please Confirm your Password',
+      (v) =>
+        /^(?=.*[a-zA-Z])(?=.*[0-9])[A-Za-z0-9]{8,18}$/.test(v) ||
+        'Password must be valid',
     ],
     checkbox: false,
   }),
 
-  methods: {
-
-  },
+  methods: {},
 }
 </script>
 
 <template>
   <Layout>
-    <v-container>
-      <div class="text-h4 secondary--text font-weight-regular">
-        Sign Up
-      </div>
-    </v-container>
+    <div class="text-h4 mb-12 secondary--text font-weight-medium">Sign Up</div>
 
-    <v-container>
-      <form
-        ref="form"
-        lazy-validation
-      >
+    <form ref="form" lazy-validation>
+      <v-text-field
+        v-model="username"
+        :error-messages="usernameErrors"
+        :rules="nameRules"
+        label="Username"
+        required
+        outlined
+        clearable
+        class="mb-4"
+      ></v-text-field>
+      <div class="d-flex mb-4">
         <v-text-field
-          v-model="username"
-          :error-messages="usernameErrors"
-          :rules="nameRules"
-          label="Username"
+          class="mr-2"
+          v-model="email"
+          :rules="emailRules"
+          label="E-mail"
           required
-          @input="$v.name.$touch()"
-          @blur="$v.name.$touch()"
+          outlined
+          clearable
         ></v-text-field>
+        <v-btn large outlined color="primary" @click="verifyEmail">
+          Send Email
+        </v-btn>
+      </div>
+      <v-text-field
+        v-model="password"
+        :error-messages="passwordErrors"
+        label="Password"
+        :rules="passwordRules"
+        required
+        outlined
+        clearable
+        class="mb-4"
+      ></v-text-field>
+      <v-text-field
+        v-model="confirm_password"
+        :error-messages="passwordErrors"
+        label="Re-enter your Password"
+        :rules="confirmPasswordRules"
+        required
+        outlined
+        clearable
+        class="mb-4"
+      ></v-text-field>
+      <div class="d-flex justify-space-between align-center mb-8">
         <div class="d-flex align-center">
-          <v-text-field
-            class="mr-2"
-            v-model="email"
-            :rules="emailRules"
-            label="E-mail"
-            required
-          ></v-text-field>
-          <v-btn small @click="verifyEmail">
-            Send Email
-          </v-btn>
-        </div>
-        <v-text-field
-          v-model="password"
-          :error-messages="passwordErrors"
-          label="Password"
-          :rules="passwordRules"
-          required
-          @input="$v.password.$touch()"
-          @blur="$v.password.$touch()"
-        ></v-text-field>
-        <v-text-field
-          v-model="confirm_password"
-          :error-messages="passwordErrors"
-          label="Re-enter your Password"
-          :rules="confirmPasswordRules"
-          required
-          @input="$v.comfirm_Password.$touch()"
-          @blur="$v.comfirm_Password.$touch()"
-        ></v-text-field>
-        <div class="d-flex">
           <v-checkbox
-            class="mr-1"
+            class="mr-2"
             v-model="checkbox"
             :error-messages="checkboxErrors"
             label="I accept "
             required
-            @change="$v.checkbox.$touch()"
-            @blur="$v.checkbox.$touch()"
           ></v-checkbox>
-          <router-link class="d-flex align-center " :to="{ path: '/signup' }">Terms and Conditions</router-link>
+          <router-link class="d-flex align-center" :to="{ path: '/signup' }"
+            >Terms and Conditions</router-link
+          >
         </div>
-        <v-btn
-          class="mr-4"
-          @click="signUp"
-        >
-          Sign up
-        </v-btn>
-      </form>
-    </v-container>
+        <v-btn depressed large color="primary" @click="signUp"> Sign up </v-btn>
+      </div>
+    </form>
 
-    <v-container>
-      <v-divider
-      ></v-divider>
-      <v-row class="mt-2 ">
-        <v-col class="col-md-6">
-          <div class="secondary--text font-weight-regular">Already have an account?</div>
-        </v-col>
-        <v-col class="col-md-2">
-          <router-link :to="{ path: '/login' }">Log in</router-link>
-        </v-col>
-      </v-row>
-    </v-container>
+    <v-divider class="mb-8"></v-divider>
 
+    <div class="d-flex">
+      <div class="secondary--text font-weight-regular mr-4">
+        Already have an account?
+      </div>
+      <router-link :to="{ name: 'login' }">Log in</router-link>
+    </div>
   </Layout>
 </template>
-
 
 <style lang="scss" scoped></style>
